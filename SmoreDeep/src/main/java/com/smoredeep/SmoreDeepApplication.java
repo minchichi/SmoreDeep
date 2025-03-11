2646d3ab2aa96565917d704292dd5a456a5ff57f
SMHRD <SMHRD@DESKTOP-OVV6183.mshome.net>
2025-03-11 11:59:03 +0900

    New Project Set

---
+++ SmoreDeep/src/main/java/com/smoredeep/SmoreDeepApplication.java
@@ -0,0 +1,13 @@
+package com.smoredeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoreDeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoreDeepApplication.class, args);
+	}
+
+}
---
+++ SmoreDeep/src/test/java/com/smoredeep/SmoreDeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoredeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoreDeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
